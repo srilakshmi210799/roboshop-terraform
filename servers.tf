efaedbcd0f47256a2c8d87b159218d09edd5a4c1
srilakshmi.g <srilakshmi210799@gmail.com>
2023-04-24 15:54:35 +0530

    terraform  servers script

--- servers.tf
+++ servers.tf
@@ -4,7 +4,7 @@ data "aws_ami" "centos" {
   name_regex       = "Centos-8-DevOps-Practice"
     }
 output "ami" {
-value = data.aws_ami.centos.id
+value = data.aws_ami.centos.image_id
 }
 resource "aws_instance" "frontend" {
   ami           = "data.aws_ami.centos.image_id"
