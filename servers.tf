7250b316d6b8613a0829db369b7feeba3e098cff
srilakshmi.g <srilakshmi210799@gmail.com>
2023-04-24 15:39:22 +0530

    terraform  servers script

--- servers.tf
+++ servers.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "centos" {
    owners           = ["973714476881"]
   most_recent      = true
-  name_regex       = "centos-8-DevOps-practice"
+  name_regex       = "Centos-8-DevOps-practice"
     }
 output "ami" {
 value = data.aws_ami.centos.id
